a6a43a868dab67b6f15bb963eb70dc61cfbc8c59
Tạ Tuấn Anh <69012511+ovftank@users.noreply.github.com>
2025-11-18 17:42:27 +0700

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,7 +16,7 @@ const robotoMono = Roboto_Mono({
 });
 
 export const generateMetadata = async () => {
-  const h = headers()
+  const h = await headers()
   const host = h.get('x-forwarded-host') || h.get('host')
   const proto = h.get('x-forwarded-proto') || 'https'
   const base = `${proto}://${host}`
